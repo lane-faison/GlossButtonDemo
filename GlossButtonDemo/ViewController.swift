1928defde2b1a55758c2f7ffbd561ff1cf175a3b
Lane Faison <lfaison@tangogroup.com>
2019-03-01 14:04:46 -0700

    Initial Commit

---
+++ GlossButtonDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlossButtonDemo
+//
+//  Created by Lane Faison on 3/1/19.
+//  Copyright © 2019 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
